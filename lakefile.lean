d8c68e5f32ebd21a85c834923c95e113599da7a6
Jonathan Cubides <prieto.jona@gmail.com>
2026-08-02 19:41:33 -0500

    chore: pin full dependency SHAs

--- lakefile.lean
+++ lakefile.lean
@@ -2,11 +2,12 @@ import Lake
 open Lake DSL
 
 package «termcolor-layout» where
-  version := v!"0.1.3"
+  version := v!"0.1.4"
   leanOptions := #[⟨`autoImplicit, false⟩, ⟨`relaxedAutoImplicit, false⟩]
 
 require «termcolor» from git
-  "https://github.com/jonaprieto/lean-termcolor.git" @ "90d49ed"
+  "https://github.com/jonaprieto/lean-termcolor.git"
+  @ "f0c0cef08a9c0c2019929856d48c6ba46458a26f"
 
 @[default_target]
 lean_lib «TermColor.Layout» where
