92af9103792196a742577548dc20d66a8e2736e3
Jonathan Cubides <prieto.jona@gmail.com>
2026-08-07 12:33:19 -0500

    refactor: narrow layout imports

--- TermColor/Layout/Width.lean
+++ TermColor/Layout/Width.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jonathan Prieto-Cubides
 -/
 
-import TermColor
+import TermColor.Text
 
 /-!
 # TermColor.Layout.Width: terminal display width
