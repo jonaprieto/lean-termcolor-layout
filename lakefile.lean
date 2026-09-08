e8d7d386ca9c926c4fe9f5fa5e584723b8d0d1b4
Jonathan Cubides <prieto.jona@gmail.com>
2026-09-08 18:12:56 -0500

    build: refresh termcolor to v1.1.6

--- lakefile.lean
+++ lakefile.lean
@@ -7,7 +7,7 @@ package «termcolor-layout» where
 
 require «termcolor» from git
   "https://github.com/jonaprieto/lean-termcolor.git"
-  @ "v1.1.5"
+  @ "v1.1.6"
 
 @[default_target]
 lean_lib «TermColor.Layout» where
